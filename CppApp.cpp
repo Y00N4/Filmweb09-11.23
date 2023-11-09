4c7d4ccd43a95546ebeda63cdec5f044539a3812
yona <126967988+Y00N4@users.noreply.github.com>
2023-11-09 22:46:46 +0100

    Add files via upload

---
+++ CppApp.cpp
@@ -0,0 +1,102 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+struct Movie
+{
+private:
+    string title;
+    int year;
+    string director;
+    float rate;
+    vector<float> userRates;
+    vector<string> comments;
+
+public:
+    Movie(string title, int year, string director, float rate)
+        : title(title), year(year), director(director), rate(rate)
+    {}
+    
+        /*Movie(string title, int year, string director, float rate){
+            this->title=title;
+            this->year = year;
+            this->director = director;
+            this->rate=rate;
+        }*/
+    
+    void DisplayMovieInfo()
+    {
+        cout<<"Title: "<<title<<endl;
+        cout<<"Year: "<<year<<endl;
+        cout<<"Director: "<<director<<endl;
+        cout<<"Rate: "<<rate<<endl;
+
+        for(int i = 0; i < comments.size(); i++)
+        {
+            cout<<i+1<<": "<<comments[i]<<endl;
+        }
+        cout<<"----------"<<endl;
+    }
+
+    void RateMovie(float yourRate)
+    {
+        userRates.emplace_back(yourRate);
+        rate=0;
+        for(int i=0;i < userRates.size();i++)
+        {
+            rate+=userRates[i];
+        }
+        rate/=userRates.size();
+    }
+
+    void AddComment(string yourComment)
+    {
+        comments.emplace_back(yourComment);
+    }
+};
+
+
+int main()
+{    
+    Movie movie1("Film 1", 1999, "Andrzej Duda", 0);
+    Movie movie2("Film 2", 1222, "Jan Matejko", 0);
+    Movie movie3("Film 3", 1410, "Mikolaj Kopernik", 0);
+    Movie movies[3] = {movie1, movie2, movie3};
+    
+    int selectedMovie = 0;
+    int selectedAction = 0;
+    float movieRate = 0;
+    string comment; 
+    while(true)
+    {
+        movies[0].DisplayMovieInfo();
+        movies[1].DisplayMovieInfo();
+        movies[2].DisplayMovieInfo();
+        cout<<"---------------------------"<<endl;
+        cout<<"Wybierz film: (1-3) 0 - wyjscie z programu";
+        cin>>selectedMovie;
+        
+        if(selectedMovie == 0)
+            break;
+        
+        cout<<endl<<"Wybierz akcje: 1 - ocen 2 - dodaj komentarz: ";
+        cin>>selectedAction;
+        
+        if(selectedAction == 1)
+        {
+            cout<<"Podaj ocene: ";
+            cin>>movieRate;
+            movies[selectedMovie-1].RateMovie(movieRate);
+        }
+        if(selectedAction == 2)
+        {
+            cout<<"Napisz komentarz: ";
+            cin>>comment;
+            movies[selectedMovie-1].AddComment(comment);
+        }
+        
+    }
+
+    cout<<"Dzieki za uzycie mojego programu";
+}
